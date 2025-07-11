b899c2ee2d9e3db3381527e79178dbc436d23a2e
Ezequiel Santos <ezequielsantosg@gmail.com>
2025-07-10 23:49:59 -0400

    Nuevos elementos
    Se agregaron los controladores para materia y calificaciones con sus respectivos modelos.

---
+++ Actividad4LengProg3/Controllers/CalificacionesController.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Actividad4LengProg3.Models
+{
+	public class CalificacionesViewModel
+	{
+		[Required(ErrorMessage = "Debe colocar su matrícula.")]
+		public string MatriculaEstudiante {  get; set; }
+
+		[Required(ErrorMessage = "Debe colocar el código de la materia.")]
+		public string CodigoMateria { get; set; }
+
+		[Required(ErrorMessage = "Debe colocar un valor válido.")]
+        [Range(0, 10)]
+		public string Nota {  get; set; }
+
+		[Required(ErrorMessage = "Debe colocar un período válido.")]
+		public string Periodo { get; set; }
+    }
+}
---
+++ Actividad4LengProg3/Controllers/MateriasController.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Actividad4LengProg3.Models
+{
+	public class MateriasViewModel
+	{
+		[Required(ErrorMessage = "Debe colocar un código válido.")]
+		public string Codigo { get; set; }
+
+		[Required]
+        [StringLength(100, ErrorMessage = "La matrícula debe tener entre 6 y 15 caracteres.")]
+		public string Nombre { get; set; }
+
+		[Require(ErrorMessage = "Debe colocar la cantidad de créditos.")]
+		[Range(1,10)]
+		public string Creditos { get; set; }
+
+		[Required(ErrorMessage = "Debe colocar la carrera.")]
+		public string Carrera
+    }
+}
